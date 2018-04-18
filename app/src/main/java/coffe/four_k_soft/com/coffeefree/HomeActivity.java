68dda224c092a258dc78546ff9240d69e092cfaf
Gerrald008-Poll <goryanskii.pasha@gmail.com>
2018-04-18 12:39:52 +0300

    data binding enabled

--- app/src/main/java/coffe/four_k_soft/com/coffeefree/HomeActivity.java
+++ app/src/main/java/coffe/four_k_soft/com/coffeefree/HomeActivity.java
@@ -1,13 +1,18 @@
 package coffe.four_k_soft.com.coffeefree;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import coffe.four_k_soft.com.coffeefree.databinding.ActivityHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = HomeActivity.class.getSimpleName();
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
     }
 }
---
+++ app/src/main/java/coffe/four_k_soft/com/coffeefree/view_model/ViewModel.java
@@ -0,0 +1,10 @@
+package coffe.four_k_soft.com.coffeefree.view_model;
+
+/**
+ *
+ * Created by goryanskiy on 18.04.18.
+ */
+
+public class ViewModel {
+
+}
